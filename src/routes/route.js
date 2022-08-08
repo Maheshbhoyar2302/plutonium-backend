ecdb6321e9d083b1e3537a1727b905abba8d7636
mahesh <mjbhoyar.2302@gmail.com>
2022-08-08 23:39:19 +0530

    this is second commit

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const abc = require('../introduction/intro')
 
+const loadash = require('lodash')
+
+
 const welcome = require('../logger/logger')
 
 const dateMonthe = require('../utils/helper')
@@ -9,18 +12,33 @@ const validation = require('../validator/formatter')
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
-    console.log('My batch is', abc.name)
-    abc.printName()
+    // console.log('My batch is', abc.name)
+    // abc.printName()
 
-    welcome.welcomeFun()
+    // welcome.welcomeFun()
 
-    dateMonthe.currentdate()
-    dateMonthe.currentMonth()
-    dateMonthe.getBatchInfo()
+    // dateMonthe.currentdate()
+    // dateMonthe.currentMonth()
+    // dateMonthe.getBatchInfo()
 
-    validation.maketrim()
-    validation.changeToLower()
-    validation.changeToUpper()
+    // validation.maketrim()
+    // validation.changeToLower()
+    // validation.changeToUpper()
+
+    //problem 4
+    const monthArr = ['January', 'February', 'March', 'April', 'May', 'June', 'July',       'August', 'September', 'October', 'November', 'December'];
+    console.log(loadash.chunk(monthArr, 3))
+
+    const oddArr = [1,3,5,7,9,11,13,15,17,19];
+    console.log(loadash.tail(oddArr))
+
+    const duplicateValues = [44,33,55,77,33]
+    console.log(loadash.union(duplicateValues))
+
+    const pairFormation = [ ["horror","The Shining"],["drama","Titanic"],["thriller","Shutter Island"],["fantasy","Pans Labyrinth"]];
+    console.log(loadash.fromPairs(pairFormation))
+
+
     res.send('My second ever api!')
 });
 
